4a9bc06008ff13b749352fa76cf2a4628833c2bf
Adam Wulf <adam.wulf@gmail.com>
2015-12-20 05:41:28 -0600

    Fixed toggling the checkbox after refreshing the issue table with a search/sort/etc

--- github-click.js
+++ github-click.js
@@ -1,6 +1,6 @@
 $(function(){
-	$(".js-issue-row input, .js-issue-row a").click(function(e) { e.stopPropagation(); });
-	$(".js-issue-row").click(function(){
+	$("body").on("click", ".js-issue-row input, .js-issue-row a",function(e) { e.stopPropagation(); });
+	$("body").on("click", ".js-issue-row",function(){
 		if(!$(this).is("input") && !$(this).is("a")){
 			var $box = $(this).find('input:checkbox:first');
 		    $box.trigger("click");
